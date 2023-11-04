e09e89fdbd04ff982c8c7487ca6d6122f5764931
Fikre <fikreg06@gmail.com>
2023-11-04 11:12:00 -0500

    react exercises are added

---
+++ src/todoList/TodoApp.jsx
@@ -0,0 +1,47 @@
+import React,{ useState } from "react";
+import './TodoApp.css';
+
+function ToDoAPP () {
+    const [tasks,setTasks] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    // const list = []; //an array to hold all value
+
+    function onClick() {
+        // list.push(inputValue); // shopping, swimming
+        setTasks([...tasks,inputValue]);
+    }
+
+    function onChange(event) {
+       setInputValue(event.target.value); // input value is shopping 
+    }
+
+    function onDelete (task) {
+        const index = tasks.indexOf(task);
+        console.log('am inside on Delete check index', index); //we use this console log to debug
+        tasks.splice(index,1);
+        setTasks([...tasks]);
+    }
+    return (
+        <>
+        <div className="wrapper">
+        <input type="text" onChange={onChange}/>
+        <button type="submit" onClick={onClick}>Add Tasks</button>
+        <br />
+        <ul className="todoList">
+            {tasks.map((task,index) => {
+                return (
+                <>
+                <li key={index} id={index}>{task}
+                <input type="checkbox"/>
+                <button onClick={(e)=>onDelete(task)}>Delete</button>
+                </li>
+                </>
+                )
+            })};
+        </ul>
+        </div>    
+        </>
+        
+    );
+}
+export default ToDoAPP
